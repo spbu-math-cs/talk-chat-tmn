73f27ae0ac0e025d7aee6a848e95542faa7c730b
Alexander Chvanov <58791253+achvanov7@users.noreply.github.com>
2020-10-25 11:07:41 +0500

    add UdpChatClient

--- client/src/ru/senin/kotlin/net/client/UdpChatClient.kt
+++ client/src/ru/senin/kotlin/net/client/UdpChatClient.kt
@@ -1,14 +1,18 @@
 package ru.senin.kotlin.net.client
 
+import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import io.ktor.utils.io.core.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import ru.senin.kotlin.net.Message
 import java.net.InetSocketAddress
 
 class UdpChatClient(private val host: String, private val port: Int) : BaseChatClient() {
     companion object {
-        private val socket: BoundDatagramSocket = TODO("Create UPD socket. Use InetSocketAddress(0) to send from random free port")
+        private val socket: BoundDatagramSocket =
+                aSocket(ActorSelectorManager(Dispatchers.IO)).udp().bind(InetSocketAddress(0))
+                //TODO("Create UPD socket. Use InetSocketAddress(0) to send from random free port")
     }
 
     override fun sendMessage(message: Message) {
